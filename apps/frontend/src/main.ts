5d00c85a0e8032f321b78bd8e0c7c1bde4daaf59
Leawind <leawind@yeah.net>
2026-08-16 17:22:05 +0800

    style: add element plus theme bridge

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -6,6 +6,7 @@ import "element-plus/es/components/input-number/style/css";
 
 import App from "./App.vue";
 import { i18n } from "./i18n";
+import "./styles/element-plus-theme.css";
 
 const platform = navigator.userAgent.includes("Mac OS X") ? "macos" : "other";
 document.documentElement.dataset.platform = platform;
